72ff2a7f6652ec492fe0148953f6ae69b9880bab
WinterMorning <754406672@qq.com>
2019-06-24 11:53:17 +0800

    登录API

---
+++ server/routes/login.js
@@ -0,0 +1,32 @@
+const Router = require('koa-router');
+const path = require('path');
+const db = require('../libs/database')
+    //登录处理
+let router = new Router();
+
+router.get('/login', async(ctx, next) => {
+    let username = ctx.query.username;
+
+    let password = ctx.query.password;
+    //数据库检索
+    let data = await ctx.db.query('SELECT username , password FROM user_table WHERE USERNAME = ?', [username]);
+    ctx.body = data;
+
+    if (data.length == 0) { //用户名不存在
+        ctx.body = {
+            state: -1,
+            msg: '用户不存在'
+        }
+    } else if (data[0].password != password) { //密码不匹配
+        ctx.body = {
+            state: -2,
+            msg: '密码错误'
+        }
+    } else {
+        ctx.body = {
+            state: 0,
+            msg: '登录成功'
+        }
+    }
+})
+module.exports = router.routes();
